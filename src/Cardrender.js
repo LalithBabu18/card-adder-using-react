63879827621b1917e1ef0e45c56b98f9cc8fde3b
Lalith Babu <51440420+LalithBabu18@users.noreply.github.com>
2019-11-01 18:47:49 +0530

    first commit

---
+++ src/Cardrender.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Card from './Card';
+
+
+export default class Cardrender extends Component {
+    arr = []
+    state = {
+        nocards: this.arr
+    }
+
+    buttonHandle = () => {
+        this.arr.push(1)
+        this.setState({
+            nocards: this.arr
+        })
+    }
+    buttonHandle2 = () => {
+        this.arr.pop(1)
+        this.setState({
+            nocards: this.arr
+        })
+    }
+
+    renderFun = () => this.state.nocards.map((val, key) => <Card />)
+
+    render() {
+        return (
+            <div className="content">
+                <div className="buttons">
+                    <button className="button1" onClick={this.buttonHandle}>Add Card</button>
+                    <br></br>
+                    <button className="button2" onClick={this.buttonHandle2}>Remove Card</button>
+                    <br></br>
+                    <button className="button3" onClick="" >View Source</button>
+                    <br></br>
+                </div>
+                <div className="card-render">
+                    <ol>
+                        <li>
+                            {this.renderFun()}
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
